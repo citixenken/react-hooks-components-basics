494d5ad97ed3f6e15b37d7ea729a1f0e42a2fbd8
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-07-12 07:03:29 +0300

    commit tree 4dcb15451d85f5752017690b940aae227878d808

--- src/Article.js
+++ src/Article.js
@@ -1,9 +1,26 @@
 import React from "react";
 
-function Article() {
-  return (
-    <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
-  );
-}
+//functional component
+//====================
+// function Article() {
+//   return (
+//     <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
+//   );
+// }
+//class-based component
+//=====================
+// class Article extends React.Component {
+//   render() {
+//     return (
+//       <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
+//     );
+//   }
+// }
+
+//arrow-function syntax
+//=====================
+const Article = () => (
+  <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
+);
 
 export default Article;
--- src/Comment.js
+++ src/Comment.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { Component } from "react";
 
-function Comment() {
-  return <div>Naturally, I agree with this article.</div>;
+// function Comment() {
+//   return <div>Naturally, I agree with this article.</div>;
+// }
+
+// const Comment = () => <div>Naturally, I agree with this article.</div>;
+
+class Comment extends Component {
+  render() {
+    return <div>Naturally, I agree with this article.</div>;
+  }
 }
 
 export default Comment;
